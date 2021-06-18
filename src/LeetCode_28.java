1d20252760d5f7619b07ac08d993ab4401f72347
jun <884720982@qq.com>
2021-06-18 09:32:47 +0800

    提交28题代码

--- src/LeetCode_28.java
+++ src/LeetCode_28.java
@@ -51,7 +51,7 @@ public class LeetCode_28
 
     public static void main(String[] args)
     {
-        System.out.println(strStr("", ""));
+        System.out.println(strStr("hello", "ll"));
     }
 }
 
