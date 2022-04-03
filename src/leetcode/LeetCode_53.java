ffc0541eda94dfa537c2580c9a85d729e75ef19d
jun <884720982@qq.com>
2022-04-03 10:17:56 +0800

    修改人：Jun 修改内容： 1、求数组最大连续和 2、用常规计算，和贪心算法解题

---
+++ src/leetcode/LeetCode_53.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+public class LeetCode_53
+{
+    public static int maxSubArray(int[] nums)
+    {
+        int result = nums[0];
+        int sum = 0;
+        for (int num : nums)
+        {
+            if (sum > 0)
+            {
+                sum += num;
+            }else
+            {
+                sum = num;
+            }
+            result = Math.max(sum,result);
+        }
+
+        return result;
+    }
+
+    /***
+     * 贪心算法求最大连续数组和
+     * @param nums
+     * @return
+     */
+    public static int greedMaxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i- 1] + nums[i], nums[i]);
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] array = new int[]{5, 4, -1, 7, 8};
+        System.out.println(maxSubArray(array));
+
+        System.out.println(greedMaxSubArray(array));
+    }
+
+}
