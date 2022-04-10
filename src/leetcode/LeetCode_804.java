6752432ce4f5aed167c124f7c9b2191c4bc632ab
jun <884720982@qq.com>
2022-04-10 10:26:19 +0800

    修改人：Jun 修改内容： 1、周六日每日一题

---
+++ src/leetcode/LeetCode_804.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LeetCode_804
+{
+    public static HashMap<String, String> arraysMap = new HashMap();
+    public static String[] arrays = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--."
+            , "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-"
+            , ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+    static
+    {
+        int index = 0;
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            arraysMap.put(String.valueOf(c), arrays[index]);
+            index++;
+        }
+    }
+
+    public static int uniqueMorseRepresentations(String[] words)
+    {
+        HashSet<String> hashSet = new HashSet<>();
+        for (String word : words)
+        {
+            char[]  charArray = word.toCharArray();
+            String mose = "";
+            for (char c : charArray)
+            {
+                mose += arraysMap.get(String.valueOf(c));
+            }
+            hashSet.add(mose);
+        }
+
+            return hashSet.size();
+    }
+
+    public static void main(String[] args)
+    {
+        String[] words = new String[]{"gin", "zen", "gig", "msg"};
+        System.out.println(uniqueMorseRepresentations(words));
+
+    }
+}
