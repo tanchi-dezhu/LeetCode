4ed3512a0289af0303dcaa065f7a1a92d4462fc7
jun <884720982@qq.com>
2022-04-10 14:42:18 +0800

    修改人：Jun 修改内容： 1、先过滤空字符串，然后返回过滤后的集合最后一个的长度

---
+++ src/leetcode/LeetCode_58.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LeetCode_58
+{
+    public static int lengthOfLastWord(String s)
+    {
+        if (s.trim().equals(""))
+        {
+            return 0;
+        }
+        String[] splits = s.split(" ");
+        List<String> collect = Arrays.stream(splits).filter(x -> !x.trim().equals("")).collect(Collectors.toList());
+        return collect.get(collect.size() - 1).length();
+    }
+
+    public static void main(String[] args)
+    {
+        String s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+    }
+}
