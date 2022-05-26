e95cad9ccd39bba5014110d1aa489782b1bd9356
jun <884720982@qq.com>
2022-05-26 20:57:19 +0800

    修改人：Jun 修改内容： 1、中序排序代码提交

---
+++ src/leetcode/simple/LeetCode_94.java
@@ -0,0 +1,64 @@
+package leetcode.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class LeetCode_94
+{
+    /** 中序排序核心代码
+     * while (root != null || !stack.isEmpty()) {
+     *     while (root != null) {
+     *         stack.push(root);
+     *         root = root.left;
+     *     }
+     *     root = stack.pop();
+     *     res.add(root.val);
+     *     root = root.right;
+     * }
+     * @param root
+     * @return
+     */
+
+    public List<Integer> inorderTraversal(TreeNode root)
+    {
+        Stack<TreeNode> stack =new Stack();
+
+        List<Integer> result = new ArrayList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+
+        return result;
+    }
+
+    public class TreeNode
+    {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode()
+        {
+        }
+
+        TreeNode(int val)
+        {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
