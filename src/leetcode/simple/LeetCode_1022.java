0d76503f9ac6232d22c74aafe2c21b3ba9289f24
jun <884720982@qq.com>
2022-05-30 20:25:39 +0800

    修改人：Jun 修改内容： 1、根据树的前中后序遍历编写

---
+++ src/leetcode/simple/LeetCode_1022.java
@@ -0,0 +1,66 @@
+package leetcode.simple;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LeetCode_1022
+{
+    public static class TreeNode
+    {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode()
+        {
+        }
+
+        TreeNode(int val)
+        {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int sumRootToLeaf(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        int val = 0, ret = 0;
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                val = (val << 1) | root.val;
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.peek();
+            if (root.right == null || root.right == prev) {
+                if (root.left == null && root.right == null) {
+                    ret += val;
+                }
+                val >>= 1;
+                stack.pop();
+                prev = root;
+                root = null;
+            } else {
+                root = root.right;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args)
+    {
+
+        TreeNode treeNodeL = new TreeNode(1, null, null);
+        TreeNode treeNodeR = new TreeNode(0, null, null);
+
+        TreeNode treeNode = new TreeNode(1, treeNodeL, treeNodeR);
+        System.out.println(sumRootToLeaf(treeNode));
+    }
+}
