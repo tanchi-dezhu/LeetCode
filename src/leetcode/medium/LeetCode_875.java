35be7eecca5ecf1ee8373f60d3a3880a2e0b265c
jun <884720982@qq.com>
2022-06-07 20:52:23 +0800

    修改人：Jun 修改内容： 1、通过二分查找，判断每堆需要多少h，全部加起来是否超过给的h数来完成

---
+++ src/leetcode/medium/LeetCode_875.java
@@ -0,0 +1,48 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class LeetCode_875
+{
+
+    public static int minEatingSpeed(int[] piles, int h)
+    {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+
+        while (left < right)
+        {
+
+            int mid = left + (right - left) / 2;
+
+            int time = calculateSum(piles, mid);
+            if (time > h)
+            {
+                // 耗时太多，说明速度太慢了，下一轮搜索区间是 [mid + 1..right]
+                left = mid + 1;
+            } else
+            {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private static int calculateSum(int[] piles, int speed)
+    {
+        int sum = 0;
+        for (int pile : piles)
+        {
+            // 求余
+            sum += (pile + speed - 1) / speed;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] array = new int[]{3, 6, 7, 11};
+        System.out.println(minEatingSpeed(array, 8));
+    }
+}
