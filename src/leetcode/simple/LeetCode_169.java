8be2cde92c5075e7134b5301cfb0856bd25e33f1
jun <884720982@qq.com>
2022-06-15 20:24:18 +0800

    修改人：Jun 修改内容： 1、求一位数的中位数，可通过>>1位来获得

---
+++ src/leetcode/simple/LeetCode_169.java
@@ -0,0 +1,63 @@
+package leetcode.simple;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LeetCode_169
+{
+    public static int majorityElement(int[] nums)
+    {
+
+        Map<Integer, List<Integer>> collect = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Integer::intValue));
+
+        int result = 0;
+        int temp = 0;
+        for (List<Integer> value : collect.values())
+        {
+            if (temp < value.size())
+            {
+                temp = value.size();
+                result = value.get(0);
+            }
+
+        }
+        return result;
+    }
+
+
+    public static int majorityElement1(int[] nums)
+    {
+
+        Map<Integer, List<Integer>> collect = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Integer::intValue));
+
+        Map.Entry<Integer, List<Integer>> majorityEntry = null;
+        for (Map.Entry<Integer, List<Integer>> entry : collect.entrySet())
+        {
+            if (majorityEntry == null || entry.getValue().size() > majorityEntry.getValue().size())
+            {
+                majorityEntry = entry;
+            }
+        }
+        return majorityEntry.getKey();
+    }
+
+    public static int majorityElement2(int[] nums)
+    {
+        Arrays.sort(nums);
+        int index = nums.length >> 1;
+        return nums[index];
+    }
+
+    public static void main(String[] args)
+    {
+        int[] array = new int[]{2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement2(array));
+
+
+        System.out.println(100 >> 1);//50
+        System.out.println(20 >> 1);//10
+        System.out.println(9 >> 1);//4
+        System.out.println(4 >> 1);//2
+        System.out.println(3 >> 1);//1
+    }
+}
