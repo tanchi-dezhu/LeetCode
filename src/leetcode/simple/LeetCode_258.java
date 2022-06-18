e1d861e8b70d127d0635332dc7948de24225bdde
jun <884720982@qq.com>
2022-06-18 10:10:02 +0800

    修改人：Jun 修改内容： 1、能够被9整除的整数，各位上的数字加起来也必然能被9整除，所以，连续累加起来，最终必然就是9。 2、不能被9整除的整数，各位上的数字加起来，结果对9取模，和初始数对9取摸，是一样的，所以，连续累加起来，最终必然就是初始数对9取摸。

--- src/leetcode/simple/LeetCode_258.java
+++ src/leetcode/simple/LeetCode_258.java
@@ -20,8 +20,29 @@ public class LeetCode_258
         return result;
     }
 
+    /**
+     * 能够被9整除的整数，各位上的数字加起来也必然能被9整除，所以，连续累加起来，最终必然就是9。
+     * 不能被9整除的整数，各位上的数字加起来，结果对9取模，和初始数对9取摸，是一样的，所以，连续累加起来，最终必然就是初始数对9取摸。
+     *
+     * @param num
+     * @return
+     */
+    public static int addDigits1(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        if (0 == num % 9)
+        {
+            return 9;
+        }
+        return num % 9;
+//        return (num-1) % 9 + 1;
+    }
+
     public static void main(String[] args)
     {
-        System.out.println(addDigits(10));
+        System.out.println(addDigits1(100));
     }
 }
