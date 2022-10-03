5c95359a8f9d40a6d51cca6338f19390cf1cc95e
jun <884720982@qq.com>
2022-10-03 09:00:57 +0800

    修改人：Jun 修改内容： 1、练习提交

--- src/leetcode/medium/LeetCode_3.java
+++ src/leetcode/medium/LeetCode_3.java
@@ -1,34 +1,56 @@
 package leetcode.medium;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class LeetCode_3
 {
     public int lengthOfLongestSubstring(String s)
     {
-        int n = s.length(), ans = 0;
-        Map<Character, Integer> map = new HashMap<>();
-//        遍历String
-        for (int end = 0, start = 0; end < n; end++)
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        int result = 0;
+
+        int start = 0;
+        for (int i = 0; i < s.length(); i++)
         {
-            char alpha = s.charAt(end);
-//            判断该数字是否重复
-            if (map.containsKey(alpha))
+            char index = s.charAt(i);
+            Integer preIndex = hashMap.get(index);
+            if (preIndex != null)
             {
-//                如果重复取出开始到重复字母的子串长度
-                start = Math.max(map.get(alpha), start);
+                start = Math.max(start, preIndex);
             }
-//            和之前的子串长度进行对比,获得最长的子串
-            ans = Math.max(ans, end - start + 1);
-            map.put(s.charAt(end), end + 1);
+            result = Math.max(result, i - start + 1);
+            hashMap.put(index, i + 1);
         }
-        return ans;
+
+        return result;
     }
+
+
+//    public int lengthOfLongestSubstring1(String s)
+//    {
+//        int n = s.length(), ans = 0;
+//        Map<Character, Integer> map = new HashMap<>();
+////        遍历String
+//        for (int end = 0, start = 0; end < n; end++)
+//        {
+//            char alpha = s.charAt(end);
+////            判断该数字是否重复
+//            if (map.containsKey(alpha))
+//            {
+////                如果重复取出开始到重复字母的子串长度
+//                start = Math.max(map.get(alpha), start);
+//            }
+////            和之前的子串长度进行对比,获得最长的子串
+//            ans = Math.max(ans, end - start + 1);
+//            map.put(s.charAt(end), end + 1);
+//        }
+//        return ans;
+//    }
 
     public static void main(String[] args)
     {
         LeetCode_3 leetCode_three = new LeetCode_3();
-        System.out.println(leetCode_three.lengthOfLongestSubstring("dengbasg"));
+        System.out.println(leetCode_three.lengthOfLongestSubstring("abcabcbb"));
     }
 }
--- src/leetcode/simple/LeetCode_1.java
+++ src/leetcode/simple/LeetCode_1.java
@@ -1,37 +1,49 @@
 package leetcode.simple;
 
+import java.util.HashMap;
+
 public class LeetCode_1
 {
     public int[] twoSum(int[] nums, int target)
     {
-        int arry[] = new int[2];
         for (int i = 0; i < nums.length; i++)
         {
-            int a = nums[i];
             for (int j = i + 1; j < nums.length; j++)
             {
-                int count = 0;
-                int b = nums[j];
-                count = a + b;
-                if (count == target)
+                if (nums[i] + nums[j] == target)
                 {
-                    arry[0] = i;
-                    arry[1] = j;
-                    return arry;
-                } else
-                {
-                    arry[0] = 0;
-                    arry[1] = 0;
+                    return new int[]{i, j};
                 }
             }
         }
-        return arry;
+
+        return null;
+    }
+
+    public int[] twoSum1(int[] nums, int target)
+    {
+        HashMap<Integer, Integer> habitable = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            int subValue = target - nums[i];
+            if (habitable.containsKey(subValue))
+            {
+                return new int[]{i, habitable.get(subValue)};
+            }
+            habitable.put(nums[i], i);
+        }
+
+        return null;
     }
 
     public static void main(String args[])
     {
         LeetCode_1 leetCode_one = new LeetCode_1();
-        int[] num = new int[]{15, 28, 36};
-        int[] end = leetCode_one.twoSum(num, 51);
+        int[] num = new int[]{15, 28, 36, 24, 34, 25};
+        int[] end = leetCode_one.twoSum1(num, 49);
+        for (int i = 0; i < end.length; i++)
+        {
+            System.out.println(end[i]);
+        }
     }
 }
