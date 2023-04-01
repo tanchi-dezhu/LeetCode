53d96c85beb537366694426f698f5b6e3240e8aa
jun <884720982@qq.com>
2023-04-01 21:13:30 +0800

    修改人：Jun 修改内容： 1、

--- src/leetcode/medium/LeetCode_377.java
+++ src/leetcode/medium/LeetCode_377.java
@@ -1,5 +1,34 @@
 package leetcode.medium;
 
+import java.time.LocalTime;
+import java.util.Arrays;
+
 public class LeetCode_377
 {
+	public static int combinationSum4(int[] nums, int target) {
+		Arrays.sort(nums);
+		int[] dp = new int[target + 1];
+		dp[0] = 1;
+		for (int i = 1; i <= target; i++) {
+			for (int num : nums) {
+				if (num > i) {
+					break;
+				}
+				dp[i] += dp[i - num];
+			}
+		}
+		return dp[target];
+	}
+
+	public static void main(String[] args)
+	{
+		int[] param = new int[]{1, 2, 3};
+
+		System.out.println(LocalTime.now());
+
+		System.out.println(combinationSum4(param, 4));
+
+		System.out.println(LocalTime.now());
+
+	}
 }
