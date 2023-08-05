aecace2e79ca7cbe9290a9e0a5e41c7a357b93ee
tanchi-dezhu <884720982@qq.com>
2023-08-05 13:55:32 +0800

    修改人：Jun 修改内容： 1、二分代码提交

---
+++ src/algorithm/binarySearch.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class binarySearch
+{
+	public static int search(int[] params, int left, int right, int param)
+	{
+		Arrays.sort(params);
+
+		int mid = (left + right) / 2;
+
+		int num = params[mid];
+
+		if (num == param)
+		{
+			return mid;
+		}
+
+		return num < param ? search(params, mid + 1, right, param) : search(params, left, mid, param);
+	}
+
+	public static void main(String[] args)
+	{
+		int[] params = new int[]{1, 4, 10, 16, 17, 18, 23, 29, 33, 40, 50, 51};
+
+
+		for (int param : params)
+		{
+			int search = search(params, 0, params.length - 1, param);
+			System.out.println(search);
+		}
+	}
+}
---
+++ src/leetcode/medium/LeetCode_76.java
@@ -0,0 +1,33 @@
+//package leetcode.medium;
+//
+//public class LeetCode_76
+//{
+//
+////	输入：s = "ADOBECODEBANC", t = "ABC"
+////	输出："BANC"
+////	解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'.
+//
+//	public static String result(String param, String sub)
+//	{
+//		String result = "";
+//
+//		char[] subChars = sub.toCharArray();
+//
+//		int left = 0;
+//		int sum = 0;
+//
+//		for (int right = 0; right < param.length(); right++)
+//		{
+//			sum += param[right];
+//			while (sum >= target)
+//			{
+//			}
+//		}
+//		return result;
+//	}
+//
+//	public static void main(String[] args)
+//	{
+//		System.out.println(result("ADOBECODEBANC", "ABC"));
+//	}
+//}
